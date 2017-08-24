8551e0cd5f9b4ba906c77bfbb862cb857d7dbad6
Hunter Harrison <hunter@Hunters-MacBook-Pro.local>
2017-08-24 14:02:03 -0500

    Finished through Async Writes in Redux

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -11,26 +11,76 @@ class ManageCoursePage extends Component {
       course: Object.assign({}, this.props.course),
       errors: {}
     };
+    this.updateCourseState = this.updateCourseState.bind(this)
+    this.saveCourse = this.saveCourse.bind(this)
   }
+
+  updateCourseState(event) {
+    const field = event.target.name;
+    let course = this.state.course;
+    course[field] = event.target.value;
+    return this.setState({course: course});
+  }
+
+  saveCourse(event){
+    event.preventDefault();
+    this.props.actions.saveCourse(this.state.course)
+    this.context.router.push('/courses');
+  }
+
+  componentWillReceiveProps(nextProps){
+    if(this.props.course.id != nextProps.course.id){
+      //Necessary to populate form when existing course is loaded directly.
+      this.setState({course: Object.assign({}, nextProps.course)})
+    }
+  }
+
   render() {
     return (
         <CourseForm
         course={this.state.course}
         errors={this.state.errors}
-        allAuthors={[]}
+        onSave={this.saveCourse}
+        allAuthors={this.props.authors}
+        onChange={this.updateCourseState}
         />
     );
   }
 }
 
 ManageCoursePage.propTypes = {
- course: PropTypes.object.isRequired
+ course: PropTypes.object.isRequired,
+ authors: PropTypes.array.isRequired,
+ actions: PropTypes.object.isRequired
 };
 
+ManageCoursePage.contextTypes = {
+  router: PropTypes.object
+};
+
+function getCourseById(courses, id) {
+  const course = courses.filter(course => course.id == id);
+  if (course.length) return course[0];
+  return null;
+}
+
 function mapStateToProps(state, ownProps){
+  const courseId = ownProps.params.id;
   let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''};
+
+  if (courseId && state.courses.length > 0){
+    course = getCourseById(state.courses, courseId)
+  }
+
+  const authorsFormattedForDropdown = state.authors.map(author => {
+    return {
+      value: author.id,
+      text: author.firstName + ' ' + author.lastName
+    }
+  })
   return{
-    course: course
+    course: course,
+    authors: authorsFormattedForDropdown
   };
 }
 
